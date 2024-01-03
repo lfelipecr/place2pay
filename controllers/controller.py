016bf97d31aea9de6dc160dddc4f0f20a95fe96d
Erick Enrique Abrego Gonzalez <programador2@grupoley.com>
2024-01-02 23:04:04 -0600

    UPD PLACE2PAY

--- controllers/controller.py
+++ controllers/controller.py
@@ -26,17 +26,17 @@ class placetopay(http.Controller):
         try:
             if(int(partner_id) <= 0):
                 pass
-            else: 
+            else:
                 if(_location_first_time == "yes"):
                     _logger.warning("si partner")
-                    _partner = http.request.env['res.partner'].sudo().browse(int(partner_id))        
+                    _partner = http.request.env['res.partner'].sudo().browse(int(partner_id))
 
                     response['country_id'] = country_id = _partner.country_id.id
                     response['state_id'] = state_id = _partner.state_id.id
                     response['county_id'] = county_id = _partner.county_id.id
-                    response['district_id'] = district_id = _partner.district_id.id                        
-                    response['neighborhood_id'] = _partner.neighborhood_id.id                
-            
+                    response['district_id'] = district_id = _partner.district_id.id
+                    response['neighborhood_id'] = _partner.neighborhood_id.id
+
             # country
             query = "select id, code, name from res_country where id = " + str(country_id)
             request.cr.execute(query)
@@ -52,7 +52,7 @@ class placetopay(http.Controller):
                 try:
                     if(int(state_id)>0):
                         query = "select id, code, name from res_country_state where id = " + str(state_id)
-                except:                    
+                except:
                     query = "select id, code, name from res_country_state where country_id = " + str(country['id'])
 
                 _logger.warning(query)
@@ -71,32 +71,32 @@ class placetopay(http.Controller):
                     if(counties):
                         response['counties'] = counties
                         _logger.warning("si counties 1")
-                        for county in counties:  
+                        for county in counties:
 
                             try:
                                 if(int(county_id)>0):
                                     query = "select id, code, name from res_country_district where county_id = " + str(county_id)
-                            except:                    
-                                query = "select id, code, name from res_country_district where county_id = " + str(counties[0]['id'])                  
-                            
+                            except:
+                                query = "select id, code, name from res_country_district where county_id = " + str(counties[0]['id'])
+
                             request.cr.execute(query)
                             districts = request.cr.dictfetchall()
                             _logger.warning(query)
                             if(districts):
                                 response['districts'] = districts
                                 _logger.warning("si districts 1")
-                                
+
                                 try:
                                     if(int(district_id)>0):
                                         query = "select id, code, name from res_country_neighborhood where district_id = " + str(district_id)
-                                except:                    
-                                    query = "select id, code, name from res_country_neighborhood where district_id = " + str(districts[0]['id'])                  
+                                except:
+                                    query = "select id, code, name from res_country_neighborhood where district_id = " + str(districts[0]['id'])
                                 request.cr.execute(query)
                                 neighborhoods = request.cr.dictfetchall()
                                 _logger.warning(query)
                                 if(neighborhoods):
                                     response['neighborhoods'] = neighborhoods
-            
+
 
         except Exception as e:
            exc_traceback = sys.exc_info()
@@ -125,7 +125,7 @@ class placetopay(http.Controller):
         return is_psql_query
 
     @http.route('/place2pay/get_document_types', methods=['POST'], type='json', auth="public", website=True)
-    def get_document_types(self, **kw):        
+    def get_document_types(self, **kw):
         response = {"document_types":None, "options":None}
         try:
             options = ""
@@ -137,13 +137,13 @@ class placetopay(http.Controller):
                 query = "select id, code, name from res_country where code = '" + str("XX") + str("'")
                 request.cr.execute(query)
                 country = request.cr.dictfetchone()
-                query = "select id, code, name from l10n_latam_document_type where country_id = " + str(country['id'])           
+                query = "select id, code, name from l10n_latam_document_type where country_id = " + str(country['id'])
             else:
                 query = "select id, code, name from l10n_latam_document_type where country_id = " + str(kw.get('country_id'))
             request.cr.execute(query)
-            document_types = request.cr.dictfetchall() 
+            document_types = request.cr.dictfetchall()
             if(document_types):
-                for document_type in document_types:                    
+                for document_type in document_types:
                     options = str(options) + str("<option value='" + str(document_type["id"]) + "' code='" + str(document_type["code"]) + "'>") + str(document_type["name"]) + str("</option>")
             response["options"] = options
             response["document_types"] = document_types
@@ -151,10 +151,10 @@ class placetopay(http.Controller):
            exc_traceback = sys.exc_info()
            raise Warning(getattr(e, 'message', repr(e))+" ON LINE "+format(sys.exc_info()[-1].tb_lineno))
         return response
-    
+
     @http.route('/place2pay/process_payment', methods=['GET'], type='http', auth="public", website=True)
-    def process_payment(self):        
-        _logger.warning("PROCESS PAYMENT")  
+    def process_payment(self):
+        _logger.warning("PROCESS PAYMENT")
         for key,value in request.session.items():
             _logger.warning(str(key) + str(value))
         _p2p = Place2Pay()
@@ -174,7 +174,7 @@ class placetopay(http.Controller):
         if(not _payment_information):
             http.request.env['payment.transaction'].sudo().cron()
         return self.ecommerce_redirection(transaction, http.request.session.p2p_order_id)
-    
+
     @http.route('/place2pay/notification', methods=['POST'], type='json', auth="public")
     def process_notification(self):
         response = request.jsonrequest
@@ -197,8 +197,8 @@ class placetopay(http.Controller):
                 self.unlink_non_p2p_payments_transactions(order['id'])
                 self._process_payment(_payment_information, order['id'])
         return None
-    
-    def get_this_acquirer(self):        
+
+    def get_this_acquirer(self):
         query = "select name, website_id,company_id, state, place2pay_login, secretkey from payment_acquirer where provider = 'place2pay' limit 1"
         request.cr.execute(query)
         acquirer = request.cr.dictfetchone()
@@ -213,7 +213,7 @@ class placetopay(http.Controller):
         acquirer = request.cr.dictfetchone()
         response = {"acquirer":acquirer}
         return response
-    
+
     # @param partner_id
     # @param online_payment
     @http.route('/place2pay/payment_request', methods=['POST'], type='json', auth="public", website=True)
@@ -223,8 +223,8 @@ class placetopay(http.Controller):
         params['online_payment'] = kw.get('online_payment')
 
         response = {"status":"FAIL", "message":"", "url":"/"}
-        
-        # GET ACQUIRER 
+
+        # GET ACQUIRER
         query = "select id, name, website_id,company_id, state, place2pay_login, secretkey from payment_acquirer where provider = 'place2pay' limit 1"
         request.cr.execute(query)
         acquirer = request.cr.dictfetchone()
@@ -247,14 +247,16 @@ class placetopay(http.Controller):
                 state_text = "pendientes"
             pending_transaction_text = str()
             for pending_transaction in pending_transactions:
-                pending_transaction_text = str("Placetopay") + str("<br>") + str("Transacción: ") + str(pending_transaction.reference) + str("<br>") + str("Referencia Nº: ") + str(pending_transaction.p2p_reference) + str("\n")                
+                pending_transaction_text = str("Placetopay") + str("<br>") + str("Transacción: ") + str(pending_transaction.reference) + str("<br>") + str("Referencia Nº: ") + str(pending_transaction.p2p_reference) + str("\n")
             return {"status":"FAIL", "message": str("Lo sentimos, tiene ") + str(transaction_text) + " " + str(state_text) + str(" por validar. Por favor esperar a la respuesta del banco emisor." + str("\n") + str(pending_transaction_text))}
 
         # FORMAT NAME & SURNAME
         _full_name = self.get_buyer_fullname(res_partner_shipping["name"])
         try:
             identification_id = str(http.request.session["identification_id"])
-            if(str(identification_id)==str("None")):
+            if res_partner_shipping["document_type"]:
+                identification_id = str(res_partner_shipping["document_type"])
+            elif(str(identification_id)==str("None")):
                 identification_id = str(res_partner_shipping["document_type"])
         except:
             identification_id = str(res_partner_shipping["document_type"])
@@ -269,9 +271,12 @@ class placetopay(http.Controller):
                             "buyer_document": str(res_partner_shipping["vat"]),
                             "buyer_identification_id": identification_id,
                             "buyer_mobile": res_partner_shipping["mobile"] if res_partner_shipping["mobile"] and len(res_partner_shipping["mobile"]) else res_partner_shipping["phone"],
+                        # PAYER
+                            "payer_document": str(res_partner_shipping["vat"]),
+                            "payer_identification_id": identification_id,
                         # PAYMENT
                             "order_name": draft_order["name"],
-                            "order_description": str("Pedido\n") + str("ID: ") +str(draft_order["id"]) + str("\n") + str("NAME: ")  + str(draft_order["name"]), 
+                            "order_description": str("Pedido\n") + str("ID: ") +str(draft_order["id"]) + str("\n") + str("NAME: ")  + str(draft_order["name"]),
                             "order_amount_currency": self.get_order_currency(draft_order["id"]),
                             "order_amount_total": draft_order["amount_total"],
                             "order_amount_untaxed": draft_order["amount_untaxed"],
@@ -297,12 +302,12 @@ class placetopay(http.Controller):
         else:
             response["message"] = str(_payment_response['message'])
         return response
-    
+
     def has_pending_payments(self, draft_order):
         transactions = self.get_order_transactions(draft_order['id'])
         _pending_transactions = []
         _has_transactions = False
-        for transaction in transactions:              
+        for transaction in transactions:
             _payment_transaction = http.request.env['payment.transaction'].sudo().browse(int(transaction['transaction_id']))
             if(_payment_transaction.p2p_request_id):
                 if(_payment_transaction.state == 'pending' or _payment_transaction.state == 'authorized'):
@@ -319,7 +324,7 @@ class placetopay(http.Controller):
                 mail_message = http.request.env['mail.message'].sudo().browse(int(_mail_message.id))
                 _logger.warning(mail_message.body)
                 if(message):
-                    mail_message.sudo().update({'body': str(mail_message.body) + str("<br>") + str(message)})                    
+                    mail_message.sudo().update({'body': str(mail_message.body) + str("<br>") + str(message)})
         else:
             mail_message_values = {
                                         'email_from': http.request.env.user.partner_id.email,
@@ -332,7 +337,7 @@ class placetopay(http.Controller):
                                         'record_name': _record_name,
                                     }
             http.request.env['mail.message'].sudo().create(mail_message_values)
-            
+
     def get_buyer(self, partner_id):
         query = "select res_partner.id, res_partner.name, res_partner.identification_id, res_partner.vat, res_partner.phone, res_partner.mobile, res_partner.email, res_partner.street, res_partner.city, res_partner.zip, res_partner.lang, res_country.name as country_name, res_country.code as country_code, res_country_state.name as state_name, res_currency.name as currency_name, res_currency.symbol as currency_symbol from res_partner left join res_country on res_country.id = res_partner.country_id left join res_country_state on res_country_state.id = res_partner.state_id left join res_currency on res_country.currency_id = res_currency.id   where res_partner.id = '"+str(partner_id)+"' limit 1"
         request.cr.execute(query)
@@ -355,7 +360,7 @@ class placetopay(http.Controller):
         request.cr.execute(query)
         order = request.cr.dictfetchone()
         return order
-    
+
     def _get_order(self, order_id):
         query = "select * from sale_order where id = "+str(order_id)
         request.cr.execute(query)
@@ -363,7 +368,7 @@ class placetopay(http.Controller):
         return order
 
     def get_order_by_transaction(self, transaction_id):
-        query = "select sale_order_id from sale_order_transaction_rel where transaction_id = " + str(transaction_id)        
+        query = "select sale_order_id from sale_order_transaction_rel where transaction_id = " + str(transaction_id)
         request.cr.execute(query)
         transaction = request.cr.dictfetchone()
         if(transaction):
@@ -396,10 +401,10 @@ class placetopay(http.Controller):
                             'state':'draft',
                             'amount':params["order"]['amount_total'],
                             'reference':str(params["order"]["name"]) + str("/") + str(params["_payment_request"]['requestId']),
-                            'currency_id':int(self.get_order_currency(params["order"]['id'], "id")),                            
-                            'state_message':str(params["_payment_request"]["status"]['message']),  
-                            'p2p_request_id':str(http.request.session.p2p_request_id),    
-                                                                                                                 
+                            'currency_id':int(self.get_order_currency(params["order"]['id'], "id")),
+                            'state_message':str(params["_payment_request"]["status"]['message']),
+                            'p2p_request_id':str(http.request.session.p2p_request_id),
+
                       }
         if(payload_response):
             transaction['p2p_reference'] = str(payload_response['payment']['reference'])
@@ -408,9 +413,9 @@ class placetopay(http.Controller):
         _transaction = request.env['payment.transaction'].sudo().create(transaction)
 
         query = "insert into  sale_order_transaction_rel (transaction_id, sale_order_id) values ("+ str(_transaction.id) +", "+ str(params["order"]['id']) +")"
-        request.cr.execute(query)  
-        return _transaction      
-    
+        request.cr.execute(query)
+        return _transaction
+
     def get_transaction(self, request_id=None):
         if(request_id):
             transaction = request.env['payment.transaction'].sudo().search([('p2p_request_id', '=', request_id)])
@@ -418,31 +423,31 @@ class placetopay(http.Controller):
             if("p2p_request_id" in http.request.session):
                 transaction = request.env['payment.transaction'].sudo().search([('p2p_request_id', '=', http.request.session.p2p_request_id)])
         return transaction
-    
+
     def get_order_transactions(self, order_id):
         query = "select * from sale_order_transaction_rel where sale_order_id = '" + str(order_id) + "'"
         request.cr.execute(query)
         transactions_rel = request.cr.dictfetchall()
         return transactions_rel
-    
+
     # Currency field isen't in order and just for lines
     def get_order_currency(self, order_id, brings="name"):
-        draft_order_lines = http.request.env['sale.order.line'].sudo().search([['order_id','=',order_id]]) 
+        draft_order_lines = http.request.env['sale.order.line'].sudo().search([['order_id','=',order_id]])
         for order_line in draft_order_lines:
             if(brings=="name"):
                 return str(order_line.currency_id.name)
             if(brings=="id"):
                 return str(order_line.currency_id.id)
-    
+
     def get_return_url(self):
         return str(request.env['ir.config_parameter'].sudo().get_param('web.base.url')) + str("/place2pay/process_payment")
-    
+
     def get_ip(self):
         return str(request.httprequest.environ['REMOTE_ADDR'])
-    
+
     def get_user_agent(self):
         return str(request.httprequest.environ['HTTP_USER_AGENT'])
-    
+
     def get_buyer_fullname(self, fullname):
         _name_parts = str(fullname).split(" ")
         name = ""
@@ -464,7 +469,7 @@ class placetopay(http.Controller):
 
     def unlink_non_p2p_payments_transactions(self, order_id):
         payment_transaction.unlink_non_p2p_payments_transactions(request.env["payment.transaction"], order_id)
-    
+
     def _process_payment(self, _payment_information, order_id):
         payment_transaction._process_payment(request.env["payment.transaction"], _payment_information, order_id)
 
--- models/place2pay.py
+++ models/place2pay.py
@@ -22,20 +22,20 @@ class Place2Pay(object):
 
     def __init__(self):
         pass
-    
+
     def get_endpoint(self, mode):
         if(mode=="test"):
             return self.webservice_test
         else:
             return self.webservice_prod
-    
+
     def set_webservice_call(self, call, mode):
         if(mode=="test"):
             self.webservice_test = str(self.webservice_test) + str(call)
         else:
             self.webservice_prod = str(self.webservice_prod) + str(call)
-    
-    def send_request(self, payload, mode="test"):        
+
+    def send_request(self, payload, mode="test"):
         headers = {'content-type': "application/json"}
         _payment_request = requests.post(self.get_endpoint(mode), json = payload, headers = headers)
         return _payment_request.json()
@@ -77,9 +77,19 @@ class Place2Pay(object):
     # @param user_agent
     def create_payment_request(self, params):
         millis = int(round(time.time() * 1000)) + int(604800000)
+        if params['buyer_identification_id'] == "CRCPF":
+            params['buyer_identification_id'] = "CPF"
         payload = {
-            'auth': self.auth(params),                        
+            'auth': self.auth(params),
             'buyer': {
+                'name': params['buyer_name'],
+                'surname': params['buyer_surname'],
+                'email': params['buyer_email'],
+                'document': params['buyer_document'],
+                'documentType': params['buyer_identification_id'],
+                'mobile': params['buyer_mobile']
+            },
+            'payer': {
                 'name': params['buyer_name'],
                 'surname': params['buyer_surname'],
                 'email': params['buyer_email'],
@@ -118,20 +128,20 @@ class Place2Pay(object):
                 }
             ]
         _logger.warning(payload)
-        return payload     
+        return payload
 
     # AUTH
     # @param login
     # @param secretkey
     def get_payment_request_information(self, params):
         payload =  {
-                        "auth": self.auth(params)                    
+                        "auth": self.auth(params)
                     }
         return payload
-           
-    
+
+
     def _generate_tran_key(self, params):
-        _line = str(params['nonce']) + str(params['seed']) + str(params['secretkey'])        
+        _line = str(params['nonce']) + str(params['seed']) + str(params['secretkey'])
         digest = hashlib.sha1(_line.encode('utf-8')).digest()
         trankey = str(base64.b64encode(digest).decode())
         return trankey
@@ -139,11 +149,11 @@ class Place2Pay(object):
     def _generate_nonce(self, length):
         nonce = uuid.uuid4().hex
         return {"nonce_bs4":str(base64.b64encode(nonce.encode()).decode()), "nonce_code":nonce}
-        
+
     def _get_seed(self):
         now = datetime.now()
         return isodate.datetime_isoformat(now) + str.format('{0:+06.2f}', -float(time.timezone) / 3600).replace('.', ':')
-    
+
     def _get_expiration_time(self):
         expiration = datetime.now() + timedelta(minutes=15)
         return isodate.datetime_isoformat(expiration) + str.format('{0:+06.2f}', -float(time.timezone) / 3600).replace('.', ':')
